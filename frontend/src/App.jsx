0d5b9716781063b5ffe8b7fd17d2d64c9915bec5
mohit <mohit44790@gmail.com>
2026-05-23 17:25:53 +0530

    admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import Home from "./pages/Home";       // create this page
+import Home from "./pages/Home";
 import Layout from "./component/Layout";
-
+import Register from "./pages/auth/Register";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminLayout from "./pages/admin/AdminLayout";
 
 const router = createBrowserRouter([
+  { path: "/login", element: <Register /> },
   {
     path: "/",
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
-     
+    ],
+  },
+  {
+    path: "/adminDashboard",
+    element: <AdminLayout />,
+    children: [
+      { index: true,              element: <AdminDashboard /> },
+  
     ],
   },
 ]);

9f8731da6670cdae7e7ed13a32ee6f7ab117461d
mohit <mohit44790@gmail.com>
2026-05-26 14:36:13 +0530

    login

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import amazon from "../../assets/amazons.png"
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
-  return (
+  const [step , setStep] = useState(1)
+  const [email,setEmail] = useState('');
+  const [password , setPassword] = useState('');
+  const [emailError , setEmailError] = useState('');
+  const [passwordError ,setPasswordError] = useState('');
+  const navigate = useNavigate();
+
+  const handleContinue = (e) =>{
+    e.preventDefault()
+    if(!email.trim()){
+      setEmailError("Enter your email or mobile number")
+      return
+
+    }
+    setEmailError('')
+    setStep(2)
+  }
+
+  /* ── Submit sign in ── */
+  const handleSignIn =(e)=>{
+    e.preventDefault()
+    if(!password.trim()){
+      setPasswordError("Enter your password")
+      return
+    }
+    setPasswordError('')
+    navigate("/")
+  }
+    return (
     <div className='min-h-screen flex flex-col items-center font-sans'>
 
       <div className='mt-6 mb-4'>

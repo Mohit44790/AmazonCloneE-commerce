e27ec1f8c0c4d75618476f2f2136477f5f9eb200
mohit <mohit44790@gmail.com>
2026-06-06 13:28:07 +0530

    allgetpro

--- frontend/src/pages/admin/products/GetAllProducts.jsx
+++ frontend/src/pages/admin/products/GetAllProducts.jsx
@@ -7,7 +7,13 @@ import {
 } from "react-icons/md";
 
 
-// const STATUS_STYLE
+const STATUS_STYLE ={
+  active : "bg-emerald-400/10 text-emerald-400 border border-emarald-400/20",
+  draft  : "bg-yellow-400/10 text-yellow-400 border border-yellow-400/20",
+  inactive : "bg-gray-400/10 text-gray-400 border border-gray-400/20",
+  out_of_stock :"bg-red-400/10 text-red-400 border border-red-400/20",
+  discontinued : "bg-purple-400/10 text-purple-400 border border-purple-400/20",
+}
 
 const GetAllProducts = () => {
   return (

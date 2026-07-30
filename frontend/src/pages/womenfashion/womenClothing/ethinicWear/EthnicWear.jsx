627302b75ded6e1de51f647e682ff7896884a99d
mohit <mohit44790@gmail.com>
2026-07-30 23:07:17 +0530

    sutta

--- frontend/src/pages/womenfashion/womenClothing/ethinicWear/EthnicWear.jsx
+++ frontend/src/pages/womenfashion/womenClothing/ethinicWear/EthnicWear.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import AmazonFashion from '../../AmazonFashion'
+import AmazonFashion from '../../AmazonFashion.jsx'
+import { ethnicWear } from '../../../../component/data/Womenfashion.js'
+import { Link } from 'react-router-dom'
 
 const EthnicWear = () => {
   return (
@@ -9,13 +11,28 @@ const EthnicWear = () => {
         <div className='w-64 border-r-2 border-gray-200 p-2'>
           <h1>Category</h1>
         </div>
-        <div className='p-2 px-2'>
-          <div>
+        <div className='p-2 px-2 w-full'>
+          <div className=''>
             <h1 className='text-lg font-bold'>Womens Ethnic Wear</h1>
             <div className='flex w-64'>
               <img src="https://m.media-amazon.com/images/G/31/img23/WA/sept/toggle/Ethnic-Toggle-2_01.jpg" alt="img1" />
               <img src="https://m.media-amazon.com/images/G/31/img23/WA/sept/toggle/Ethnic-Toggle-2_02.jpg" alt="img2" />
             </div>
+
+            <div className='bg-gray-200 p-4'>
+
+            <div className='flex bg-white'>
+              {ethnicWear.map((item,id)=>(
+                <div className='text-center w-32 p-2'>
+                  <Link key={id} to={item.link}>
+                  <img src={item.image} alt={item.label} />
+                  <p>{item.label}</p>
+                  </Link>
+                </div>
+              ))}
+
+            </div>
+              </div>
           </div>
         </div>
       </div>

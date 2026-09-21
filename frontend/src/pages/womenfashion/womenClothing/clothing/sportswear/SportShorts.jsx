50022666d3d2a6e0cc93a18384cd912651ad7bef
mohit <mohit44790@gmail.com>
2026-09-21 17:08:01 +0530

    sportwear

--- frontend/src/pages/womenfashion/womenClothing/clothing/sportswear/SportShorts.jsx
+++ frontend/src/pages/womenfashion/womenClothing/clothing/sportswear/SportShorts.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { IoIosArrowBack } from 'react-icons/io'
+import { Link } from 'react-router-dom'
 
 const SportShorts = () => {
   return (
-    <div>Shorts</div>
+    <div>
+   
+        <div className='flex flex-col bg-white md:flex-row gap-2 p-2'>
+           <div className="w-64 flex flex-col gap-2">
+              <h1 className="font-semibold text-sm">Category</h1>
+              <Link to="/women/clothing" className="flex items-center  text-sm">
+               <IoIosArrowBack /> Women
+              </Link>
+              <Link to="/women/clothing/sports-wear" className="flex items-center  text-sm">
+                <IoIosArrowBack /> Sportswear
+              </Link>
+              <h1 className="font-semibold text-sm px-4">Active Dresses</h1>
+           </div>
+            <div className='flex-1 min-w-0 w-full'>
+             <h1 className="font-semibold text-4xl px-4">Featured categories</h1>
+           </div>
+         </div>
+       </div>
+    
   )
 }
 
--- frontend/src/pages/womenfashion/womenClothing/clothing/sportswear/TrackJackets.jsx
+++ frontend/src/pages/womenfashion/womenClothing/clothing/sportswear/TrackJackets.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const TrackJackets = () => {
   return (
-    <div>TrackJackets</div>
+     <div>
+
+     <div className='flex flex-col bg-white md:flex-row gap-2 p-2'>
+        <div className="w-64 flex flex-col gap-2">
+           <h1 className="font-semibold text-sm">Category</h1>
+           <Link to="/women/clothing" className="flex items-center  text-sm">
+            <IoIosArrowBack /> Women
+           </Link>
+           <Link to="/women/clothing/sports-wear" className="flex items-center  text-sm">
+             <IoIosArrowBack /> Sportswear
+           </Link>
+           <h1 className="font-semibold text-sm px-4">Active Dresses</h1>
+        </div>
+         <div className='flex-1 min-w-0 w-full'>
+          <h1 className="font-semibold text-4xl px-4">Featured categories</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 

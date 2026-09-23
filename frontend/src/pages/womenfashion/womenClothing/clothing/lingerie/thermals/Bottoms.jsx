11a7b7ff40e894690aa5f157bc5e350086106648
mohit <mohit44790@gmail.com>
2026-09-23 23:06:07 +0530

    thermals

--- frontend/src/pages/womenfashion/womenClothing/clothing/lingerie/thermals/Bottoms.jsx
+++ frontend/src/pages/womenfashion/womenClothing/clothing/lingerie/thermals/Bottoms.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { IoIosArrowBack } from 'react-icons/io'
+import { Link } from 'react-router-dom'
 
 const Bottoms = () => {
   return (
-    <div>Bottoms</div>
+    <div>
+
+     <div className='flex flex-col bg-white md:flex-row gap-2 p-2'>
+        <div className="w-64 flex flex-col gap-2">
+           <h1 className="font-semibold text-sm">Category</h1>
+           <Link to="/women/clothing" className="flex items-center  text-sm">
+            <IoIosArrowBack /> Women
+           </Link>
+           <Link to="/women/clothing/thermals" className="flex items-center  text-sm">
+             <IoIosArrowBack /> Thermals
+           </Link>
+           <h1 className="font-semibold text-sm px-4">Bottoms</h1>
+        </div>
+         <div className='flex-1 min-w-0 w-full'>
+          <h1 className="font-semibold text-4xl px-4">Featured categories</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 

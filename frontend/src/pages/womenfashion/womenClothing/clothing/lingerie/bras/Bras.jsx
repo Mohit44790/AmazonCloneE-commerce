020af9e42f6e5fda13b99a232249b0ebdcf4f084
mohit <mohit44790@gmail.com>
2026-09-25 23:44:31 +0530

    brs

--- frontend/src/pages/womenfashion/womenClothing/clothing/lingerie/bras/Bras.jsx
+++ frontend/src/pages/womenfashion/womenClothing/clothing/lingerie/bras/Bras.jsx
@@ -10,15 +10,35 @@ const Bras = () => {
         <div className="w-64 flex flex-col gap-2">
            <h1 className="font-semibold text-sm">Category</h1>
            <Link to="/women/clothing" className="flex items-center  text-sm">
+            <IoIosArrowBack /> Clothing & Accessories
+           </Link>
+           <Link to="/women/clothing" className="flex items-center  text-sm">
             <IoIosArrowBack /> Women
            </Link>
-           <Link to="/women/clothing/sports-wear" className="flex items-center  text-sm">
-             <IoIosArrowBack /> Sportswear
+           <Link to="/women/clothing/lingerie" className="flex items-center  text-sm">
+             <IoIosArrowBack /> Lingerie
            </Link>
            <h1 className="font-semibold text-sm px-4">Bras</h1>
+             <div className="px-6">
+                 <Link to="/women/lingerie/bras/adhesive" className="flex items-center  text-sm">
+            Adhesive Bras
+           </Link>
+                 <Link to="/women/clothing/sports-wear/innerwear/sports-bras" className="flex items-center  text-sm">
+            Sports Bras
+           </Link>
+                 <Link to="/women/lingerie/bras/mastectomy" className="flex items-center  text-sm">
+            Mastectomy Bras
+           </Link>
+                 <Link to="/women/lingerie/bras/everyday" className="flex items-center  text-sm">
+            Everyday Bras
+           </Link>
+             </div>
         </div>
          <div className='flex-1 min-w-0 w-full'>
-          <h1 className="font-semibold text-4xl px-4">Featured categories</h1>
+          <h1 className="font-semibold text-xl">Results</h1>
+          <p className="text-sm text-gray-600">
+            Check each product page for other buying options. Price and other details may vary based on product size and colour.
+          </p>
         </div>
       </div>
     </div>
